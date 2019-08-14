707aab244f09b1c26fbbff4813adfb9e035c7f89
p1tDrujba <p.borshchev@gmail.com>
2019-08-14 10:25:26 +0300

    Изучена теория 1-14
    Изучена теория:
    let и const
    arrow-функции
    параметры по умолчанию
    rest параметр
    spread оператор
    дуструктуризация объектов
    деструктуризация массивов

--- react-apps/todo/src/App.js
+++ react-apps/todo/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Добро должно быть с кулаками
         </p>
         <a
           className="App-link"

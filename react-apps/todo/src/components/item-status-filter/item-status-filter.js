0d20190c97d3ed2fd245f8ccfc0a3dda9861fcdf
p1tDrujba <p.borshchev@gmail.com>
2019-09-11 21:39:28 +0300

    Создали компоненты-классы, вместо функций

--- react-apps/todo/src/components/item-status-filter/item-status-filter.js
+++ react-apps/todo/src/components/item-status-filter/item-status-filter.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <button type="button"
-                    className="btn btn-info">Все</button>
-            <button type="button"
-                    className="btn btn-outline-secondary">Активные</button>
-            <button type="button"
-                    className="btn btn-outline-secondary">Завершенные</button>
-        </div>
-    );
-};
+export default class ItemStatusFilter extends Component {
 
-export default ItemStatusFilter;
+    render() {
+        return (
+            <div className="btn-group">
+                <button type="button"
+                        className="btn btn-info">Все</button>
+                <button type="button"
+                        className="btn btn-outline-secondary">Активные</button>
+                <button type="button"
+                        className="btn btn-outline-secondary">Завершенные</button>
+            </div>
+        );
+    }
+}
